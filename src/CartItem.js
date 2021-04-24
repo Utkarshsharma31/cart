015105fe0386faab0767fb5a596770bfb7015f64
utkarsh sharma <info@novoinvent.com>
2021-04-24 22:12:43 +0530

    Adding state object

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,33 +1,63 @@
-import React from 'react'
+import React from "react";
 
 class CartItem extends React.Component {
-    render() {
-        return (
-            <div className="cart-item">
-                <div className="left-block">
-                <img style={Styles.image} />
-                </div>
-                <div className="right-block">
-                   <div style={{fontSize:25}}>Phone</div>
-                   <div style={{color:'#777'}}>Rs 999</div>
-                   <div style={{color:'#777'}}>Qty: 1</div> 
-                   <div className="cart-item-actions">
-                    {/* Buttons    */}
-                   </div>
-                </div>
-                
-            </div>
-        );
-    }
-}
-const Styles={
-    image  : {
-        height:110,
-        width:110,
-        borderRadius:4,
-        background:'#ccc',
-    }
+  constructor() {
+    super();
+    this.state = {
+      price: 999,
+      title: "Mobile Phone",
+      qty: 2,
+      img: "",
+    };
+  }
+  increaseQuantity =() =>{
+    console.log(this.state)
+  }
+  decreaseQuantity  =() =>{
+    console.log(this.state)
+  }
+  render() {
+    const { price, title, qty } = this.state;
+    return (
+      <div className="cart-item">
+        <div className="left-block">
+          <img style={Styles.image} />
+        </div>
+        <div className="right-block">
+          <div style={{ fontSize: 25 }}>{title}</div>
+          <div style={{ color: "#777" }}>Rs {price}</div>
+          <div style={{ color: "#777" }}>Qty: {qty}</div>
+          <div className="cart-item-actions">
+            <img
+              alt="increase"
+              className="action-icons"
+              src="https://www.flaticon.com/svg/vstatic/svg/992/992651.svg?token=exp=1619266629~hmac=12080decae03eb76742599eef7ade1ab"
+              onClick={this.increaseQuantity}
+            />
 
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://www.flaticon.com/svg/vstatic/svg/992/992683.svg?token=exp=1619266571~hmac=bfeb4390323c5608ff958af30ea22801"
+              onClick={this.decreaseQuantity}
+            />
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://www.flaticon.com/svg/vstatic/svg/1214/1214428.svg?token=exp=1619266664~hmac=c8ee45cef073dbf2a2fbb95587ea16f7"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-export default CartItem
-
+const Styles = {
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    background: "#ccc",
+  },
+};
+export default CartItem;
